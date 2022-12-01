8aa9a8b8a105366d45c290814e7021aeaf3bfd83
Beatrix Klebe <bklebe@mbta.com>
2022-12-01 11:00:49 -0500

    feat: day 1 part 2

--- first/src/main.rs
+++ first/src/main.rs
@@ -2,6 +2,23 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("no input.txt found!");
-    let max = input.split("\n\n").map(|chunk| chunk.split("\n").map(|line| line.parse::<i32>().unwrap()).sum::<i32>()).max().unwrap();
+    let calories = count_calories(&input);
+    let max = calories.last().unwrap();
     println!("{}", max);
+    let top3 = calories.iter().rev().take(3).sum::<i32>();
+    println!("{}", top3);
+}
+
+fn count_calories(input: &String) -> Vec<i32> {
+    let mut calories: Vec<i32> = input
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .split("\n")
+                .map(|line| line.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect();
+    calories.sort();
+    calories
 }
